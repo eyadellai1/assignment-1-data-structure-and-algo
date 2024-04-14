1296fbfd820f59d24dc0fb83a9b3fabd04f7e53d
della <della@ED>
2024-04-15 02:07:32 +0300

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace  CustomDataList
+{
+         class Program
+    {
+             static void Main(string[] args)
+        {
+            Students student = new Students
+            {
+                StudentFirstName = "Eya",
+                StudentLastName = "Dellai",
+                StudentNumber = "15010",
+                StudentAverageScore = 3.15f
+            };
+            CustomDataList dataList = new CustomDataList(10);
+
+            dataList.Add(student, "2");
+            dataList.DisplayList();
+
+
+
+
+        }
+    }
+}
